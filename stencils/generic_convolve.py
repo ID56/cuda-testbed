07e27b713a51a151ea0361fc24c679e1fde4d23b
ID56 <mashrurmahmud@iut-dhaka.edu>
2020-08-28 20:16:01 +0600

    2D Convolution with 3x3 stencil kernel.

--- stencils/generic_convolve.py
+++ stencils/generic_convolve.py
@@ -1,61 +1,56 @@
 import numpy as np
 import numba as nb
 from timeit import timeit
-from scipy.ndimage.morphology import binary_dilation
-
+from scipy.signal import convolve2d
 
 @nb.stencil(standard_indexing=("b",), neighborhood = ((-1, 1),(-1,1)))
 def _stcl_kernel(a, b):
-    """Stencil kernel for 3x3 binary dilation."""
-
+    """Stencil kernel for 3x3 2D convolution."""
+    c = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if a[i,j] and b[i+1,j+1]:
-                return True
-    return False
+            c += a[i,j]*b[i+1,j+1]
+    return c
 
 @nb.njit(parallel=True, cache=True, fastmath=True)
-def dilate3x3(a, b):
-    """JIT wrapper for _stcl_kernel."""
+def conv3x3(a, b):
+    """Wrapper for _stcl_kernel."""
     return _stcl_kernel(a,b)
 
 
 def test_equality(N):
     print(f'Testing equality with {N}x{N} array.')
-    a = np.zeros((N,N), dtype=bool)
     np.random.seed(0)
-    a[tuple(np.random.choice(N,N//2)), tuple(np.random.choice(N,N//2))] = 1
+    a = np.random.choice(256, (N,N))
     b = np.array([[0, 1, 0],
                   [1, 0, 1],
-                  [0, 1, 0]], dtype = bool)
+                  [0, 1, 0]])
     
     # padding is needed to handle border values
     p = 1
     a_ = np.pad(a, (p,p), constant_values=(0,0))
-    out = dilate3x3(a_, b)[p:-p,p:-p]
-    out_ = binary_dilation(a, b)
+    out = conv3x3(a_, b)[p:-p,p:-p]
+    out_ = convolve2d(a, b, 'same')
     print('Equality:', np.array_equal(out, out_))
 
 
 def test_runtime(N):
     print(f'Testing runtime with {N}x{N} array.')
-    a = np.zeros((N,N), dtype=bool)
     np.random.seed(56)
-    a[tuple(np.random.choice(N,N//2)), tuple(np.random.choice(N,N//2))] = 1
-
+    a = np.random.choice(256, (N,N))
     b = np.array([[0, 1, 0],
                   [1, 0, 1],
-                  [0, 1, 0]], dtype = bool)
+                  [0, 1, 0]])
     
     # padding is needed to handle border values
     p = 1
     a_ = np.pad(a, (p,p), constant_values=(0,0))
 
     nl = 10
-    print('Scipy binary dilation:', timeit(lambda: binary_dilation(a, b), number=nl))
-    print('JIT compiled stencil: ', timeit(lambda: dilate3x3(a_, b), number=nl))
-    # roughly 2x gain
-    
+    print('Scipy convolve2d:', timeit(lambda: convolve2d(a, b), number=nl))
+    print('JIT compiled stencil: ', timeit(lambda: conv3x3(a_, b), number=nl))
+    # roughly 7x gain
+
 if __name__ == '__main__':
     N = 1000
     test_equality(N)
